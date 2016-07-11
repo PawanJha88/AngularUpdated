f4b86f2f94c514e1f2f58b8a80927cc58c6456ca
kiran paturi <kpaturi@smarttuition.com>
2016-07-11 14:44:51 +0530

    added code

--- app/common/mainCtrl.js
+++ app/common/mainCtrl.js
@@ -1,5 +1,17 @@
 angular.module("flipzon")
     .controller("mainCtrl", ["$scope", function ($scope) {
         $scope.companyName = "FlipZon";
+        $scope.navBarUrl = 'app/common/navbar.html';
+        $scope.contentUrl = "app/home/home.html";
+
+        $scope.loadContent = function (name) {
+            if (name == 'login') {
+                $scope.contentUrl = "app/login/login.html"
+            } else if (name == "register") {
+                $scope.contentUrl = "app/register/register.html"
+            } else {
+                $scope.contentUrl = "app/home/home.html";
+            }
+        };
 
     }]);
